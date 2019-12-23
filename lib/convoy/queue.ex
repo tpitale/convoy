ba4ad962eeb20d20670acb7405d8303214add861
John Giffin <john.giffin@offgrid-electric.com>
2019-12-22 20:57:30 -0700

    replace async get_records on attach with synchronous call

--- lib/convoy/queue.ex
+++ lib/convoy/queue.ex
@@ -149,8 +149,9 @@ defmodule Convoy.Queue do
       %{stream: opts.stream, handler_id: handler_id}
     )
 
-    send(self(), :poll)
-    {:noreply, {queue, %{opts | handlers: new_handlers}}}
+    IO.puts("GETTING RECORDS")
+    {_records, new_state} = get_records(10, {queue, %{opts | handlers: new_handlers}})
+    {:noreply, new_state}
   end
 
   def handle_cast({:detach, handler_id}, {queue, opts}) do
